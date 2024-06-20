a8a7892b6c81c21d1a0d9525b1d109a34fe4a842
Hani-Mohammed <hani.20@hotmail.co.uk>
2024-06-19 23:33:58 -0600

    Adding Searcher

--- src/array/Main.java
+++ src/array/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        /*************Testing array operations*************/
+        print("*************Testing array operations*************");
         Integer[] data = {1, 2, 3, 4, 5, 8, 9};
         Array<Integer> intArray = new Array<>(data, 9);
         intArray.push(10);
@@ -36,7 +37,58 @@ public class Main {
         
         print(studentsData);
         
-
+        /*************Testing Searcher*************/
+        print("*************Testing Linear Searcher*************");
+        Searcher<Integer> searcher = new Searcher<>(data);
+        int value = 4;
+        int index = searcher.linearSearch(value);
+        if(index == -1)
+            print(value+" not found");
+        else print(value+" found at "+ index);
+        value = -1;
+        index = searcher.linearSearch(value);
+        if(index == -1)
+            print(value+" not found");
+        else print(value+" found at "+ index);
+        print("*************Testing Binary Searcher*************");
+        value = 4;
+        index = searcher.binarySearch(value);
+        if(index == -1)
+            print(value+" not found");
+        else print(value+" found at "+ index);
+        value = -1;
+        index = searcher.binarySearch(value);
+        if(index == -1)
+            print(value+" not found");
+        else print(value+" found at "+ index);
+        /*************Testing Linear Search with Objects*************/
+        print("*************Testing Linear Search with Objects *************");
+        Car[] cars = new Car[10];
+        cars[0] = new Car("SKJ321J2JJ12", "Toyota",2001, 240);
+        cars[1] = new Car("GSFW45131411", "kIA",2000, 220);
+        cars[2] = new Car("GFSART432245", "Honda",2008, 250);
+        cars[3] = new Car("SKJ321J2JJ12", "BMW",2009, 260);
+        cars[4] = new Car("KGFDSWR42425", "Mercedes",2001, 260);
+        cars[5] = new Car("GDSGDG522245", "Jeep",1998, 250);
+        cars[6] = new Car("SSG35252GSS4", "Ford",1980, 220);
+        cars[7] = new Car("GSGBGD352522", "Volkes Wagen",2003, 230);
+        cars[8] = new Car("SGJJDRY55255", "Volvo",2005, 220);
+        cars[9] = new Car("KFYEE5325662", "Tesla",2020, 260);
+        
+        Searcher<Car> carSearcher = new Searcher<>(cars);
+        Car testCar = new Car("SKJ321J2JJ12", "BMW",2009, 260);
+        index = carSearcher.linearSearch(testCar);
+        if(index == -1)
+            print(testCar+" not found");
+        else print(testCar+" found at "+ index);
+        /*************Testing Binary Search with Objects*************/
+        print("*************Testing Binary Search with Objects *************");
+        Car[] sortedCars = {cars[6],cars[5],cars[1],cars[0],cars[4],cars[7],cars[8],cars[2],cars[3],cars[9]};
+        carSearcher.setData(sortedCars);
+        index = carSearcher.binarySearch(testCar);
+        if(index == -1)
+            print(testCar+" not found");
+        else print(testCar+" found at "+ index);
     }
     
     public static void print(Object o)
